9a3917486b017b1ddd27e36def2dddaab0f5b69d
VHSgunzo <wosganzo@gmail.com>
2025-01-05 18:00:57 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$(readlink -f "$0" 2>/dev/null)" 2>/dev/null)"
-echo "= create rootfs.tar.gz"
+echo "= create rootfs.tar"
 tar -cf rootfs.tar -C . rootfs
 echo "= create archlinux package"
 makepkg -fsCc --noconfirm
